c796259c7dbb8b7fa8a43ebd89cf96c6b7ab7037
xelor <matthias.drescher@bytePassion.de>
2022-12-03 20:53:05 +0100

    prepare for Day 4

---
+++ AdventCoding/Day04/Input_4.cs
@@ -0,0 +1,4 @@
+public static class Input_4 {
+    public static string input = 
+@"";
+}
---
+++ AdventCoding/Day04/Solution_4-1.cs
@@ -0,0 +1,18 @@
+public class Solution_4_1 : ISolution
+{
+    public void run()
+    {
+        Console.WriteLine("Starting");
+
+        var input = Input_4.input.Split('\n');
+
+        var sum = 0;
+
+        for(int i=0; i<input.Length; i++) {
+            
+          
+        }
+
+        Console.WriteLine($"done! Sum: {sum}");
+    }    
+}
