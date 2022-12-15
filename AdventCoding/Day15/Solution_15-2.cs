83443ad45423eabb77f16ead6dd35f620c51d7d5
xelor <matthias.drescher@bytePassion.de>
2022-12-15 21:07:03 +0100

    another cleanup

--- AdventCoding/Day15/Solution_15-2.cs
+++ AdventCoding/Day15/Solution_15-2.cs
@@ -23,50 +23,29 @@ public class Solution_15_2 : ISolution
                                  new Coord() {X=beaconX, Y=beaconY}));
         }
 
-        var squares = CreateSqares(new Coord() {X=0, Y=0}, coordMax, 100);
-        var notFullyCoveredSquares = new List<Square>();
-
-        foreach(var square in squares) {        
-            if (!rhombs.Any(r => IsSquareFullyCoveredByRhomb(square, r))) {
-                notFullyCoveredSquares.Add(square);
-            }
-        }
-
-        var notFullyCoveredSquares2 = new List<Square>();
-
-        foreach(var square in notFullyCoveredSquares) {
-            var subsquares = CreateSqares(square.P1, 40000, 100);
-            foreach(var subSquare in subsquares) {
-                if (!rhombs.Any(r => IsSquareFullyCoveredByRhomb(subSquare, r))) {
-                    notFullyCoveredSquares2.Add(subSquare);
+        var overallLenght = coordMax;
+        var inputSquareList = new List<Square>() { new Square(new Coord(), overallLenght) };
+        var resultSquareList = new List<Square> ();
+        
+        while (true) {
+            foreach(var square in inputSquareList) {
+                var subsquares = CreateSqares(square.P1, overallLenght, (overallLenght > 100) ? 100 : 4);
+                foreach(var subSquare in subsquares) {
+                    if (!rhombs.Any(r => IsSquareFullyCoveredByRhomb(subSquare, r))) {
+                        resultSquareList.Add(subSquare);
+                    }
                 }
             }
+
+            if (resultSquareList.Count == 1)
+                break;
+
+            inputSquareList = resultSquareList;
+            resultSquareList = new List<Square>();
+            overallLenght /= 100;
         }
 
-        var notFullyCoveredSquares3 = new List<Square>();
-
-        foreach(var square in notFullyCoveredSquares2) {
-            var subsquares = CreateSqares(square.P1, 400, 100);
-            foreach(var subSquare in subsquares) {
-                if (!rhombs.Any(r => IsSquareFullyCoveredByRhomb(subSquare, r))) {
-                    notFullyCoveredSquares3.Add(subSquare);
-                }
-            }
-        }
-
-        var notFullyCoveredSquares4 = new List<Square>();
-
-        foreach(var square in notFullyCoveredSquares3) {
-            var subsquares = CreateSqares(square.P1, 4, 4);
-            foreach(var subSquare in subsquares) {
-                var anyRhombCoversThisSquare = rhombs.Any(r => IsSquareFullyCoveredByRhomb(subSquare, r));
-                if (!anyRhombCoversThisSquare) {
-                    notFullyCoveredSquares4.Add(subSquare);
-                }
-            }
-        }
-
-        var result = notFullyCoveredSquares4.First();
+        var result = resultSquareList.First();
         Console.WriteLine($"DONE! Frequency: {result.P1.X*4000000+result.P1.Y}");
     }  
 
@@ -84,31 +63,24 @@ public class Solution_15_2 : ISolution
         return resultList;
     }
 
-    private bool IsSquareFullyCoveredByRhomb(Square s, Rhomb r) {
+    private static bool IsSquareFullyCoveredByRhomb(Square s, Rhomb r) {
         return IsPointWithinRhomb(s.P1, r) &&
                IsPointWithinRhomb(s.P2, r) &&
                IsPointWithinRhomb(s.P3, r) &&
                IsPointWithinRhomb(s.P4, r);
     }
 
-    private bool IsPointWithinRhomb(Coord c, Rhomb r) {
-        var dist = GetManhattenDistance(c, r.Center);
-        return dist <= r.Distance;
+    private static bool IsPointWithinRhomb(Coord c, Rhomb r) {
+        return GetManhattenDistance(c, r.Center) <= r.Distance;
     }
 
-    private long GetManhattenDistance(Coord a, Coord b) {
+    public static long GetManhattenDistance(Coord a, Coord b) {
         return Math.Abs(a.X-b.X) + Math.Abs(a.Y-b.Y);
     } 
     
-
-    private class Coord {
+    public class Coord {
         public long X { get; set; }
         public long Y { get; set; }
-
-        public override string ToString()
-        {
-            return $"({X}|{Y})";
-        }
     }
 
     private class Square {
@@ -126,20 +98,14 @@ public class Solution_15_2 : ISolution
         public Coord P4 { get; }
     }
 
-
-
     private class Rhomb {
 
         public Rhomb(Coord sensor, Coord beacon) {
             Center = sensor;
-            Distance = GetManhattenDistance(sensor, beacon);
+            Distance = Solution_15_2.GetManhattenDistance(sensor, beacon);
         }
 
         public Coord Center { get; }
         public long Distance { get; }
-
-        private long GetManhattenDistance(Coord a, Coord b) {
-            return Math.Abs(a.X-b.X) + Math.Abs(a.Y-b.Y);
-        } 
     }
 }
