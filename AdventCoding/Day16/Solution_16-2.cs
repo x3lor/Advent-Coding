a201bb07f0d65f91c626aa50f1743724cc7536f2
xelor <matthias.drescher@bytePassion.de>
2022-12-17 07:52:29 +0100

    day 16 successful!

--- AdventCoding/Day16/Solution_16-2.cs
+++ AdventCoding/Day16/Solution_16-2.cs
@@ -1,6 +1,6 @@
 using System.Text;
 
-public class Solution_16_1 : ISolution
+public class Solution_16_2 : ISolution
 {
     public void run()
     {
@@ -48,30 +48,66 @@ public class Solution_16_1 : ISolution
             }
         }
 
+        
         var startNode = GetNodeById(nodes, "AA");
-        var flowNodes = new List<Node> {
-            GetNodeById(nodes, "JX"),
-            GetNodeById(nodes, "IK"),
-            //GetNodeById(nodes, "WH"),
-            GetNodeById(nodes, "CF"),
+        
+        var myFlowNodes = new List<Node> {            
+            GetNodeById(nodes, "WL"),  //
+            GetNodeById(nodes, "WH"),  //                     
+            GetNodeById(nodes, "HG"),  //         
+            GetNodeById(nodes, "IT")   //
+        };
+
+         
+        var toChooseFrom = new Node[] {
+            GetNodeById(nodes, "DS"),
             GetNodeById(nodes, "QQ"),
             GetNodeById(nodes, "VK"),
-            //GetNodeById(nodes, "WL"),
-            GetNodeById(nodes, "DS"),
+            GetNodeById(nodes, "JX"),
+            GetNodeById(nodes, "IK"),
+            GetNodeById(nodes, "CF"),
             GetNodeById(nodes, "WM"),
             GetNodeById(nodes, "EZ"),
             GetNodeById(nodes, "LB"),
             GetNodeById(nodes, "BV"),
-            //GetNodeById(nodes, "HG"),
             GetNodeById(nodes, "UY"),
-            //GetNodeById(nodes, "IT")
-        };
-        var flowNodeCount = flowNodes.Count;
-        var result = new Result(flowNodeCount);
-        Permute(flowNodes, 0, flowNodeCount-1, result, startNode, distanceMatrix); 
+        }; // 11
 
-        Console.WriteLine($"done! Result: {result.Max}");
+        int max = 0;
+
+        for (int elephantItems=6; elephantItems<=9; elephantItems++) {
+
+            Console.WriteLine(elephantItems);
+
+            var items = Combinations.CombinationsRosettaWoRecursion(toChooseFrom, elephantItems);
+
+            foreach (var comb in items) {
+
+                var elephantNodes = comb;
+                var myNodes = myFlowNodes.ToList();
+                myNodes.AddRange(toChooseFrom.Where(n => !elephantNodes.Contains(n)));
+
+                var best = Get2Result(myNodes, elephantNodes.ToList(), startNode, distanceMatrix);
+                if (best > max)
+                    max = best;
+            }
+
+        }
+
+        Console.WriteLine($"done! Result: {max}");
     }    
+
+    private int Get2Result(List<Node> myFlowNodes, List<Node> elFlowNodes, Node startNode, int[,] distanceMatrix) {
+        var myFlowNodeCount = myFlowNodes.Count;
+        var myResult = new Result(myFlowNodeCount);
+        Permute(myFlowNodes, 0, myFlowNodeCount-1, myResult, startNode, distanceMatrix);
+
+        var elFlowNodeCount = elFlowNodes.Count;
+        var elResult = new Result(elFlowNodeCount);
+        Permute(elFlowNodes, 0, elFlowNodeCount-1, elResult, startNode, distanceMatrix); 
+
+        return myResult.Max+elResult.Max;
+    }
 
     private string NodeToString(Node n) {
         if (n.Flow > 0) {
@@ -143,19 +179,20 @@ public class Solution_16_1 : ISolution
         var currentFlow = 0;
 
         for (var nodeIndex=0; nodeIndex<flownodes.Count; nodeIndex++) {
+
             var nextNode = flownodes[nodeIndex];
             var stepsToNextNode = distanceMatrix[currentNode.Index, nextNode.Index];
 
             for (int i=0; i<stepsToNextNode; i++) {
                 timer++;
-                if (timer > 30)
+                if (timer > 26)
                     break;
 
                 summedFlow += currentFlow;
             }
 
             timer++; 
-            if (timer > 30)
+            if (timer > 26)
                 break;
 
             summedFlow += currentFlow;
@@ -163,8 +200,8 @@ public class Solution_16_1 : ISolution
             currentNode = nextNode;
         }
 
-        if (timer < 30) {
-            summedFlow += currentFlow * (30-timer);
+        if (timer < 26) {
+            summedFlow += currentFlow * (26-timer);
         }
         return summedFlow;
     }
@@ -244,3 +281,203 @@ public class Solution_16_1 : ISolution
         }
     }
 }
+
+/*
+fixed approach
+
+var myFlowNodes = new List<Node> {            
+            GetNodeById(nodes, "DS"),
+            GetNodeById(nodes, "QQ"),
+            GetNodeById(nodes, "VK"),
+            GetNodeById(nodes, "WL"),  //
+            GetNodeById(nodes, "WH"),  //                     
+            GetNodeById(nodes, "HG"),  //         
+            GetNodeById(nodes, "IT")   //
+        };
+
+        var myFlowNodeCount = myFlowNodes.Count;
+        var myResult = new Result(myFlowNodeCount);
+        Permute(myFlowNodes, 0, myFlowNodeCount-1, myResult, startNode, distanceMatrix); 
+
+        var elFlowNodes = new List<Node> {
+            GetNodeById(nodes, "JX"),
+            GetNodeById(nodes, "IK"),
+            GetNodeById(nodes, "CF"),
+            GetNodeById(nodes, "WM"),
+            GetNodeById(nodes, "EZ"),
+            GetNodeById(nodes, "LB"),
+            GetNodeById(nodes, "BV"),
+            GetNodeById(nodes, "UY"),
+        };
+
+        var elFlowNodeCount = elFlowNodes.Count;
+        var elResult = new Result(elFlowNodeCount);
+        Permute(elFlowNodes, 0, elFlowNodeCount-1, elResult, startNode, distanceMatrix);
+
+*/
+
+/*
+var result = new List<string>();
+
+        foreach(var node in nodes) {
+            foreach(var link in node.Links) {
+                var newEntry = $"{NodeToString(node)} --- {NodeToString(link)}";
+                var opposite = $"{NodeToString(link)} --- {NodeToString(node)}";
+
+                if (!result.Contains(opposite))
+                    result.Add(newEntry);
+            }
+        }
+
+        foreach(var r in result) {
+            Console.WriteLine(r);
+        }
+*/
+
+/*
+graph TD
+ZT ---> QQ-11
+ZT ---> DS-9
+JX-22 ---> CI
+JX-22 ---> ZH
+JX-22 ---> UR
+EM ---> WH-12
+EM ---> IT-10
+AA ---> EQ
+AA ---> QD
+AA ---> NP
+AA ---> ZP
+AA ---> KX
+HW ---> CI
+HW ---> BV-13
+IK-8 ---> ET
+IK-8 ---> NU
+IK-8 ---> ZO
+IK-8 ---> XL
+IK-8 ---> QD
+HA ---> WQ
+HA ---> LB-17
+WH-12 ---> EM
+WH-12 ---> LW
+KU ---> BV-13
+KU ---> CF-18
+QD ---> AA
+QD ---> IK-8
+CF-18 ---> KU
+CF-18 ---> JT
+CF-18 ---> CM
+VC ---> AD
+VC ---> UY-5
+JT ---> CF-18
+JT ---> ZH
+QQ-11 ---> ZT
+ZP ---> EZ-16
+ZP ---> AA
+LI ---> LB-17
+LI ---> CM
+CI ---> HW
+CI ---> JX-22
+VK-6 ---> YM
+VK-6 ---> LC
+VK-6 ---> HE
+VK-6 ---> NU
+VK-6 ---> TI
+WL-20 ---> LW
+WL-20 ---> TO
+TI ---> VK-6
+TI ---> YW
+NU ---> VK-6
+NU ---> IK-8
+DS-9 ---> NP
+DS-9 ---> MV
+DS-9 ---> FR
+DS-9 ---> ZT
+DS-9 ---> YW
+HE ---> VK-6
+HE ---> EQ
+ZH ---> JT
+ZH ---> JX-22
+TO ---> MT
+TO ---> WL-20
+CM ---> LI
+CM ---> CF-18
+WM-14 ---> MO
+WM-14 ---> WQ
+WM-14 ---> EC
+WM-14 ---> RN
+EZ-16 ---> RT
+EZ-16 ---> RZ
+EZ-16 ---> ZP
+PB ---> YM
+PB ---> UY-5
+XL ---> IK-8
+XL ---> MS
+LB-17 ---> LI
+LB-17 ---> HA
+LB-17 ---> ON
+LB-17 ---> UR
+LB-17 ---> AD
+WQ ---> WM-14
+WQ ---> HA
+BV-13 ---> KU
+BV-13 ---> RT
+BV-13 ---> HW
+BV-13 ---> MO
+BV-13 ---> EH
+RN ---> WM-14
+RN ---> RZ
+LW ---> WH-12
+LW ---> WL-20
+NP ---> AA
+NP ---> DS-9
+MT ---> TO
+MT ---> HG-19
+ET ---> IK-8
+ET ---> EC
+HG-19 ---> MT
+MV ---> UY-5
+MV ---> DS-9
+RT ---> BV-13
+RT ---> EZ-16
+ON ---> LB-17
+ON ---> EH
+MO ---> BV-13
+MO ---> WM-14
+UY-5 ---> PB
+UY-5 ---> BR
+UY-5 ---> MS
+UY-5 ---> VC
+UY-5 ---> MV
+UR ---> JX-22
+UR ---> LB-17
+YM ---> PB
+YM ---> VK-6
+RZ ---> RN
+RZ ---> EZ-16
+AD ---> VC
+AD ---> LB-17
+EH ---> ON
+EH ---> BV-13
+EQ ---> AA
+EQ ---> HE
+KX ---> AA
+KX ---> BR
+BR ---> UY-5
+BR ---> KX
+LC ---> VK-6
+LC ---> IT-10
+YW ---> TI
+YW ---> DS-9
+EC ---> ET
+EC ---> WM-14
+IT-10 ---> LC
+IT-10 ---> EM
+MS ---> UY-5
+MS ---> XL
+FR ---> DS-9
+FR ---> ZO
+ZO ---> FR
+ZO ---> IK-8
+  
+
+*/
--- AdventCoding/Program.cs
+++ AdventCoding/Program.cs
@@ -1,4 +1,4 @@
-﻿ISolution solution1 = new Solution_16_1();
+﻿ISolution solution1 = new Solution_16_2();
 solution1.run();
 // ISolution solution2 = new Solution_16_2();
 // solution2.run();
@@ -8,3 +8,13 @@ solution1.run();
 //var visualizer = new VisualizerDay07();
 //visualizer.DoIt("Folders and files with sizes.txt", true);
 //visualizer.DoIt("Folders and files without sizes.txt", false);
+
+// var pre = new int[] {
+//     0,1,2,3,4,5,6,7,8,9,10
+// };
+
+// var items = Combinations.CombinationsRosettaWoRecursion(pre, 9);
+
+// foreach(var item in items) {
+//     Console.WriteLine(string.Join(',', item));
+// }
