8cabae34bbb5676c22461dcfeb080e429418e4fd
xelor <matthias.drescher@bytePassion.de>
2023-12-01 13:01:22 +0100

    cleanup

--- AdventCoding/Program.cs
+++ AdventCoding/Program.cs
@@ -1,21 +1,3 @@
-﻿ISolution solution1 = new Solution_1_2_23();
-solution1.run();
-// ISolution solution2 = new Solution_16_2();
-// solution2.run();
-
-
-//visualization day 7
-//var visualizer = new VisualizerDay07();
-//visualizer.DoIt("Folders and files with sizes.txt", true);
-//visualizer.DoIt("Folders and files without sizes.txt", false);
-
-// var pre = new int[] {
-//     0,1,2,3,4,5,6,7,8,9,10
-// };
-
-// var items = Combinations.CombinationsRosettaWoRecursion(pre, 9);
-
-// foreach(var item in items) {
-//     Console.WriteLine(string.Join(',', item));
-// }
+﻿ISolution solution = new Solution_1_2_23();
+solution.run();
 
