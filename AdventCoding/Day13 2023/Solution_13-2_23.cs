f6699a2b932cf41497d422673590f4e1fe24d898
xelor <matthias.drescher@bytePassion.de>
2023-12-13 13:39:53 +0100

    cleanup

--- AdventCoding/Day13 2023/Solution_13-2_23.cs
+++ AdventCoding/Day13 2023/Solution_13-2_23.cs
@@ -27,18 +27,12 @@ public class Solution_13_2_23 : ISolution
     }
 
     public class Grid {
-
-        private static int GridNr = 0;
-
+        
         private readonly List<string> grid;
         private readonly List<string> grid_transpose;
-        private readonly int gridNr;
-
+        
         public Grid(List<string> grid) {
-
-            gridNr = GridNr;
-            GridNr++;
-
+            
             this.grid = grid;
             grid_transpose = new List<string>();
 
@@ -55,30 +49,13 @@ public class Solution_13_2_23 : ISolution
 
             var oldNumHorizontal = GetReflectionNumberForAGrid(grid, 0);
             var oldNumVertical   = GetReflectionNumberForAGrid(grid_transpose, 0);
-            
-            if (oldNumHorizontal != 0) {                                    
-                var newHorizontal = GetReflectionNumber(grid, oldNumHorizontal);
-                if (newHorizontal != 0) {                    
-                    return 100*newHorizontal;
-                } else {                   
-                    var newVertical = GetReflectionNumber(grid_transpose, 0);
-                    if (newVertical != 0) {                        
-                        return newVertical;
-                    }
-                }                
-            }
-            if (oldNumVertical != 0) {                            
-                var newVertical = GetReflectionNumber(grid_transpose, oldNumVertical);
-                if (newVertical != 0) {
-                    return newVertical;
-                } else {                    
-                    var newHorizontal = GetReflectionNumber(grid, 0);
-                    if (newHorizontal != 0) {
-                        return 100*newHorizontal;
-                    }
-                }
-            }            
-            return 0;
+                                                 
+            var newHorizontal = GetReflectionNumber(grid, oldNumHorizontal);               
+            if (newHorizontal != 0)                     
+                return 100*newHorizontal;
+                                   
+            var newVertical = GetReflectionNumber(grid_transpose, oldNumVertical);                                              
+            return newVertical;                                          
         }
        
         private static int GetReflectionNumber(List<string> grid, int oldNum) {
@@ -86,25 +63,24 @@ public class Solution_13_2_23 : ISolution
             for (int yChange=0; yChange<grid.Count; yChange++) {
                 for (int xChange=0; xChange<grid[0].Length; xChange++) {
 
-                    if (grid[yChange][xChange] == '#')
-                        grid[yChange] = grid[yChange].Remove(xChange, 1).Insert(xChange, ".");
-                    else 
-                        grid[yChange] = grid[yChange].Remove(xChange, 1).Insert(xChange, "#");
-
+                    SwapGridItem(grid, xChange, yChange);
                     var result = GetReflectionNumberForAGrid(grid, oldNum);
+                    SwapGridItem(grid, xChange, yChange);
 
-                    if (grid[yChange][xChange] == '#')
-                        grid[yChange] = grid[yChange].Remove(xChange, 1).Insert(xChange, ".");
-                    else 
-                        grid[yChange] = grid[yChange].Remove(xChange, 1).Insert(xChange, "#");
-
-                    if (result != 0 && result != oldNum) {                                        
+                    if (result != 0) {                                        
                         return result;
                     }                        
                 }
             }
 
             return 0;
+        }
+
+        private static void SwapGridItem(List<string> grid, int x, int y) {
+             if (grid[y][x] == '#')
+                grid[y] = grid[y].Remove(x, 1).Insert(x, ".");
+            else 
+                grid[y] = grid[y].Remove(x, 1).Insert(x, "#");
         }
 
         private static int GetReflectionNumberForAGrid(List<string> grid, int oldNr) {
@@ -118,24 +94,20 @@ public class Solution_13_2_23 : ISolution
 
                 var foundReflection = true;
 
-                for (var j=0; j<=i; j++) {
-                    
+                for (var j=0; j<=i; j++) {                    
                     if (i+j+1 >= grid.Count) {
                         break;
                     }
-
                     if (grid[i-j] != grid[i+j+1])
                     {
                         foundReflection = false;
                         break;
                     }
                 }
-
                 if (foundReflection) {
                     return i+1;
                 }
             }
-
             return 0;
         }
     }
