0fbba00b472f93b26dc177413bc0d9e002679444
xelor <matthias.drescher@bytePassion.de>
2023-12-15 08:50:15 +0100

    cleanup

--- AdventCoding/Day15 2023/Solution_15-2_23.cs
+++ AdventCoding/Day15 2023/Solution_15-2_23.cs
@@ -12,7 +12,7 @@ public class Solution_15_2_23 : ISolution
         foreach (var part in Input_15_23.input.Split(',')) {
 
             if (part[^1] == '-') {
-                var toRemove = part.Remove(part.Length-1);
+                var toRemove = part[..^1];
                 var box = boxes[GetHash(toRemove)];
                 var existingBox = box.FirstOrDefault(s => s.StartsWith(toRemove));
                 if (existingBox != null) {                
@@ -22,7 +22,7 @@ public class Solution_15_2_23 : ISolution
 
             if (part[^2] == '=') {
                 var element     = part[..^2];
-                var focalLength = part.Substring(part.Length-1, 1);
+                var focalLength = part[^1];
                 var box = boxes[GetHash(element)];
                 var existingBox = box.FirstOrDefault(s => s.StartsWith(element));
                 if (existingBox != null) {                    
