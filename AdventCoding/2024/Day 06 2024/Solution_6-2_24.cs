09db4a632f01be99f55e177ff5362d422f1b52ea
Dschilli <matthias.drescher@bytePassion.de>
2024-12-06 15:05:55 +0100

    day 6

---
+++ AdventCoding/2024/Day 06 2024/Solution_6-2_24.cs
@@ -0,0 +1,127 @@
+public class Solution_6_2_24 : ISolution
+{
+    public void run()
+    {
+        Console.WriteLine("Starting ... ");
+
+        var grid = new Grid(Input_6_24.input);
+        var result = grid.Start();
+        Console.WriteLine($"\ndone! Sum: {result}");
+    }
+
+    private class Grid {
+
+        private char[,] grid;
+        private int xStart;
+        private int yStart;
+        private int width;
+        private int height;
+
+        public Grid(string input) 
+        {
+            var lines = input.Split('\n').ToList();
+            height = lines.Count;
+            width = lines[0].Length;
+            grid = CreateCharArray(lines, height, width);
+
+            for (int y=0; y<lines.Count; y++) 
+            {
+                var x = lines[y].IndexOf('^');
+                if (x != -1) 
+                {
+                    yStart = y;
+                    xStart = x;
+                    break;
+                }
+            }
+        }
+
+        static char[,] CreateCharArray(List<string> strings, int height, int width)
+        {
+            char[,] charArray = new char[height, width];
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    charArray[y, x] = strings[y][x];
+                
+            return charArray;
+        }
+
+        private enum Direction { UP, RIGHT, DOWN, LEFT }
+
+        public int Start() {
+
+            int counter=0;
+
+            for (int obstacleY = 0; obstacleY < height; obstacleY++) {
+                Console.Write($"\rProgress: {obstacleY} of {height-1}");
+                for (int obstacleX = 0; obstacleX < width; obstacleX++) {
+                    if (Test(obstacleX, obstacleY)) {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+
+        private bool Test(int obstacleX, int obstacleY) 
+        {
+            if ((obstacleX == xStart && obstacleY == yStart) || grid[obstacleY, obstacleX] == '#') 
+            {
+                return false;
+            }
+
+            grid[obstacleY, obstacleX] = '#';
+
+            var direction = Direction.UP;
+            var currentX = xStart;
+            var currentY = yStart;
+            var maxSteps = width*height/2;
+
+            for (int steps = 0;steps < maxSteps; steps++) {
+
+                if (currentX < 0 || currentX >= width || currentY < 0 || currentY >= height)
+                {
+                    grid[obstacleY, obstacleX] = '.';
+                    return false;
+                }
+
+                if (!CanMove(currentX, currentY, direction)) 
+                {
+                    direction = (Direction)(((int)direction+1)%4);
+                    continue;
+                }
+
+                switch (direction) 
+                {
+                    case Direction.UP:    currentY--; break;
+                    case Direction.RIGHT: currentX++; break;
+                    case Direction.DOWN:  currentY++; break;
+                    case Direction.LEFT:  currentX--; break;
+                }
+            }
+
+            grid[obstacleY, obstacleX] = '.';
+            return true;
+        }
+
+        private bool CanMove(int x, int y, Direction dir) {
+
+            int nextX = x;
+            int nextY = y;
+
+            switch (dir) {
+                case Direction.UP:    nextY--; break;
+                case Direction.RIGHT: nextX++; break;
+                case Direction.DOWN:  nextY++; break;
+                case Direction.LEFT:  nextX--; break;
+            }
+
+            if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= height)
+                return true;
+
+            return grid[nextY, nextX] != '#';
+        }
+    }
+}
